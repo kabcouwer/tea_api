a024b296d81c73efff33bd6de858d8cb428342ab
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-11-01 16:43:13 -0600

    Add default status as active

--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2021_11_01_181305) do
   create_table "subscriptions", force: :cascade do |t|
     t.string "title"
     t.float "price"
-    t.integer "status"
+    t.integer "status", default: 0
     t.integer "frequency"
     t.bigint "tea_id"
     t.bigint "customer_id"
